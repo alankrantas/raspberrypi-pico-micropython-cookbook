80c9a74b70b8465c007bfc31367ddb3633e1fe83
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-04-28 23:15:21 +0800

    Update game_of_life_singlecore.py

--- game-of-life/game_of_life_singlecore.py
+++ game-of-life/game_of_life_singlecore.py
@@ -21,9 +21,8 @@ SDA_PIN  = const(26)
 X      = WIDTH // DOT_SIZE
 Y      = HEIGHT // DOT_SIZE
 TOTAL  = X * Y
-board  = bytearray([0 if urandom.randint(0, (100 // RAND_PCT) - 1) else 1
-                    for _ in range(TOTAL)])
-buffer = bytearray([])
+board  = [0 if urandom.randint(0, (100 // RAND_PCT) - 1) else 1 for _ in range(TOTAL)]
+buffer = [0 for _ in range(TOTAL)]
 gen    = 0
 
 
@@ -38,35 +37,29 @@ print('Conway\'s Game of Life: matrix size {} x {}'.format(X, Y))
 
 def calculate_next_gen():
     global board
-    buffer = bytearray([0] * TOTAL)
     for i in range(TOTAL):
         group = board[i-1:i+2] + \
                 board[(i-1-X)%TOTAL:(i+2-X)%TOTAL] + \
                 board[(i-1+X)%TOTAL:(i+2+X)%TOTAL]
         cells = sum(group)
         if not board[i]:
-            if cells in BIRTH:
-                buffer[i] = 1
+            buffer[i] = 1 if (cells in BIRTH) else 0
         else:
-            if (cells - 1) in SURVIVAL:
-                buffer[i] = 1
-    board = buffer
+            buffer[i] = 1 if ((cells - 1) in SURVIVAL) else 0
+    board[:] = buffer
 
 
 def display_board():
-    display.fill(0)
     for i in range(TOTAL):
-        if board[i]:
-            display.fill_rect((i % X) * DOT_SIZE,
-                              (i // X) * DOT_SIZE,
-                              DOT_SIZE, DOT_SIZE, 1)
+        display.fill_rect((i % X) * DOT_SIZE,
+                          (i // X) * DOT_SIZE,
+                          DOT_SIZE, DOT_SIZE, board[i])
     display.show()
 
 
 gen, start, t = 0, 0, 0
 
 while True:
-    gc.collect()
     gen += 1
     print('Gen {}: {} cell(s) ({} ms)'.format(gen, sum(board), t))
     display_board()
@@ -74,4 +67,3 @@ while True:
     start = utime.ticks_ms()
     calculate_next_gen()
     t = utime.ticks_diff(utime.ticks_ms(), start)
-
